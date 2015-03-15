caee2f042cb839016203be2309e6d604928c6cb1
sabin512 <sabin512@users.noreply.github.com>
2015-03-15 12:33:20 -0400

    Use entry for path, switch to boxes for layout

--- findDupesGtk.py
+++ findDupesGtk.py
@@ -1,20 +1,28 @@
 from gi.repository import Gtk
 import findDupes
 
-PATH_TO_PROCESS = '/Music/PunkRock/Lagwagon'
+PATH_TO_PROCESS = '/Music'
 
 class FindDupesGtk(Gtk.Window):
     def __init__(self, dupe_finder):
         self.dupe_finder = dupe_finder
         Gtk.Window.__init__(self, title='Gtk Front-end for findDupes')
-        
-        self.grid = Gtk.Grid()
-        self.grid.set_column_homogeneous(True)
-        self.grid.set_row_homogeneous(True)
-        self.add(self.grid)
 
+        self.set_default_size(640, 480) #nostalgia sizing
+        
+        main_box = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=5)
+        self.add(main_box)
+
+        top_box = Gtk.Box(spacing=5)
+        main_box.pack_start(top_box, False, False, 0)
+        
+        self.path_entry = Gtk.Entry()
+        self.path_entry.set_text(PATH_TO_PROCESS)
+        top_box.pack_start(self.path_entry, True, True, 0)
+        
         self.scan_button = Gtk.Button('Scan')
         self.scan_button.connect('clicked', self.on_scan_button_clicked)
+        top_box.pack_start(self.scan_button, False, False, 0)
         
         self.dupes_liststore = Gtk.ListStore(str)             
         self.treeview = Gtk.TreeView(self.dupes_liststore)
@@ -24,16 +32,16 @@ class FindDupesGtk(Gtk.Window):
         self.scrollable_treelist = Gtk.ScrolledWindow()
         self.scrollable_treelist.set_vexpand(True)
         self.scrollable_treelist.set_hexpand(True)
-
-        self.grid.attach(self.scan_button, 0, 0, 100, 24)
-        self.grid.attach_next_to(self.scrollable_treelist, self.scan_button, Gtk.PositionType.BOTTOM, 640, 200)
         
         self.scrollable_treelist.add(self.treeview)
+
+        main_box.pack_start(self.scrollable_treelist, True, True, 0)
+        
         self.show_all()
 
     def on_scan_button_clicked(self, widget):
         self.show_message('Scanning...')
-        self.dupe_finder.scan(PATH_TO_PROCESS)
+        self.dupe_finder.scan(self.path_entry.get_text())
         self.dupe_finder.list_dict()
 
     def show_message(self, message):
